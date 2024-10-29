b49cda63ce3c351fdf34ef84b10a6b411fac091c
Javi Calonge <javicalongem@gmail.com>
2024-10-29 15:20:11 +0100

    Optimizar código

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,38 +1,4 @@
 <script setup>
-
-//LA MANERA MODERNA DE HACERLO: con setup en la etiqueta script
-
-// const transactions = [
-//   { id: 1, text: "Lidl", amount: -89},
-//   { id: 2, text: "Factura Luz", amount: -49.98},
-//   { id: 3, text: "Nómina", amount: 2400}
-// ]
-
-//UNA MANERA DE HACERLO
- //export default {
-  // data() {
-  //   return {
-  //     transactions: [
-  //       { id: 1, text: "Lidl", amount: -89},
-  //       { id: 2, text: "Factura Luz", amount: -49.98},
-  //       { id: 3, text: "Nómina", amount: 2400}
-  //     ]
-  //   }
-  // }
-
-//OTRA MANERA DE HACERLO
- //export default {
-  // setup() {
-  //   const transactions = [
-  //     { id: 1, text: "Lidl", amount: -89},
-  //     { id: 2, text: "Factura Luz", amount: -49.98},
-  //     { id: 3, text: "Nómina", amount: 2400}
-  //   ];
-  //   return {
-  //     transactions
-  //   }
-  // }
-
   import { defineProps } from 'vue';
 
   const props = defineProps({
